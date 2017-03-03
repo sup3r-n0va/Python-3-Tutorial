cdb8920d8ff7974ed8eee7198b38cead6785ebdd
sup3r-n0va <sup3rn0va@protonmail.com>
2017-03-03 12:09:37 +0000

    new file:   basics.py

---
+++ basics.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import os
+
+
+#this is a string
+#you use print to display on the screen
+print("Hello World") 
+
+
+# this is a comment 
+''' This is a 
+    multiline comment '''
+
+
+#this is a variable you use to store values
+# this variable contains a string which is just a sequence of characters
+name = "sup3rn0va"
+
+#to print variable you do the following
+print(name)
+
+
