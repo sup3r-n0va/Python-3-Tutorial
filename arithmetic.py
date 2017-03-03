3c9bd2c0f7cbc22a845caa334878dfefe8fd627a
sup3r-n0va <sup3rn0va@protonmail.com>
2017-03-03 12:19:56 +0000

    new file:   arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+'''These are modules which are some of the
+functions in python program need'''
+import random
+import sys
+import os	
+
+
+'''These are different arithmetics in python program'''
+
+#this is addition in python
+print("5 + 2 =", 5+2)
+#this is subtraction in python
+print("5 - 2 =", 5-2)
+#This is multiplication in python
+print("5 * 2 =", 5*2)
+#This is division in python
+print("5 / 2 =", 5/2)
+#This is how you calculate the remainder in python
+print("5 % 2 =", 5%2)
+#This is how you calulate exponential in python
+print("5 ** 2 =", 5**2)
+#This is how you calculate floor divsion so rounds down 
+print("5 // 2 =", 5//2)
+
